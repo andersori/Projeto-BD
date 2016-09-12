9e1dac5f43388e740a6925709414136b8fbafeb1
Anderson Soriano <andersori7@gmail.com>
2016-09-12 13:34:56 -0300

    Adicionando descrição ao codigo
    Descrição do funcionário.

--- Banco de dados/headers/tabelas.h
+++ Banco de dados/headers/tabelas.h
@@ -1,14 +1,14 @@
 #ifndef TABELAS_H_INCLUDED
 #define TABELAS_H_INCLUDED
 
-typedef enum{
-    true = 1, false = 0
+typedef enum{               //Definindo um enum para usar o "true" e o "false", pois no 'c' não existe o tipo de dado "boolean"
+    true = 1, false = 0     //O valor de "true" é 1 e o "false" é 0
 }boolean;
 
-typedef struct{
-    unsigned int codigo;
-    char* nome;
-    char* tipo;
+typedef struct{             //Estrutura da tabela Cliente
+    unsigned int codigo;    //O codigo é um número inteiro não negativo
+    char* nome;             //Cadeia de caracteres
+    char* tipo;             //Cadeia de caracteres
 }Cliente;
 
 typedef struct{

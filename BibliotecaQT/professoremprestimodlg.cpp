e0366f9b1c9078bab3ff043953db93e23a20c766
douglasrz <douglasrz22@gmail.com>
2016-12-08 15:56:06 -0300

    emprestimo funcionando

--- BibliotecaQT/professoremprestimodlg.cpp
+++ BibliotecaQT/professoremprestimodlg.cpp
@@ -29,9 +29,19 @@ ProfessorEmprestimoDLG::~ProfessorEmprestimoDLG()
     delete ui;
 }
 void ProfessorEmprestimoDLG::buscarLivroProf(){
-    ui->tituloLivroPr->setEnabled(true);
     QSqlDatabase bd = ConnectionDB::get_bd();
     codLivroAl=ui->lineEdit->text();
+
+    //VERIFICANDO SE TEM ALGUM LIVRO COM ESSE ID JÁ EMPRESTADO
+    QString cons("select *from emprestimo where id_exemplar="+codLivroAl);
+    QSqlQuery veriIdEmprestado=bd.exec(cons);
+    veriIdEmprestado.next();
+    if(!veriIdEmprestado.value(0).isNull()){
+        QMessageBox::warning(this,tr("LIVRO JÁ FOI EMPRESTADO"),tr("ESTE ID PERTENCE A UM LIVRO AINDA EM EMPRÉSTIMO"));
+        return;
+    }
+
+    //PEGANDO O TITULO PELO ID
     QString qry("SELECT titulo FROM publicacao WHERE id = "+codLivroAl);
     QSqlQuery resul=bd.exec(qry);
     if(resul.next()){
@@ -42,13 +52,30 @@ void ProfessorEmprestimoDLG::buscarLivroProf(){
     }else{
         QMessageBox::warning(this,tr("ERRO"),tr("NÃO EXISTE LIVRO COM ESTE CÓDIGO, CADASTRADO."));
         ui->lineEdit->clear();
-        ui->tituloLivroPr->setEnabled(false);
     }
 }
+
 void ProfessorEmprestimoDLG::buscarProf(){
-    ui->nomeProf->setEnabled(true);
     QSqlDatabase bd = ConnectionDB::get_bd();
     siape=ui->lineEdit_2->text();
+
+    //pegando o id do usuario pela matricula dele
+    QString consulId("SELECT id_usuario FROM professor WHERE siape="+siape);
+    QSqlQuery IdUsuario=bd.exec(consulId);
+    IdUsuario.next();
+    QString id=IdUsuario.value(0).toString();
+
+    //VERIFICO SE O CARA JÁ TEM 15 EMPRESTIMO, MAXIMO PERMITIDO
+    QString qtdEmprestimo("select count(*) from emprestimo where id_usuario="+id+" AND data_devolvido is null");
+    QSqlQuery quantidade=bd.exec(qtdEmprestimo);
+    quantidade.next();
+    int quant= quantidade.value(0).toInt();
+    if(quant>=5){//SE TIVER DEZ EMPRESTIMO, CANCELA
+        QMessageBox::warning(this,tr("ERRO"),tr("USUÁRIO TEM 15 OU MAIS EMPRÉSTIMOS EM ABERTO"));
+        return;
+    }
+
+    //PEGANDO O NOME PELO SIAPE DIGITADO
     QString qry("SELECT U.nome FROM professor as P, usuario as U WHERE P.id_usuario=U.id AND P.siape= "+siape);//PRECISA ALTERAR
     QSqlQuery resul=bd.exec(qry);
     if(resul.next()){
@@ -58,13 +85,13 @@ void ProfessorEmprestimoDLG::buscarProf(){
     }else{
         QMessageBox::warning(this,tr("ERRO"),tr("NÃO EXISTE PROFESSOR COM ESTE SIAPE, CADASTRADO."));
         ui->lineEdit_2->clear();
-        ui->nomeProf->setEnabled(false);
     }
 }
 
 void ProfessorEmprestimoDLG::efetuarEmprestimo(){
     QSqlDatabase bd = ConnectionDB::get_bd();
 
+    //PEGANDO O ID DO USUARIO PELO SIAPE DIGITADO
     QString qry("SELECT id_usuario FROM professor WHERE siape="+siape);
     QSqlQuery busca=bd.exec(qry);
     busca.next();
@@ -85,8 +112,6 @@ void ProfessorEmprestimoDLG::efetuarEmprestimo(){
                 ui->efetuarEmprestimoButton_3->setEnabled(false);
                 ui->empButtonProf2->setEnabled(false);
                 ui->lineEdit_2->setEnabled(false);
-                ui->nomeProf->setEnabled(false);
-                ui->tituloLivroPr->setEnabled(false);
                 return;
             }
     }
@@ -103,8 +128,8 @@ void ProfessorEmprestimoDLG::efetuarEmprestimo(){
     tent.bindValue(":id_exemplar",codLivroAl);
     tent.bindValue(":id_usuario",id_usuario);
     tent.bindValue(":data_emprestimo",data);
-    tent.bindValue(":data_devedevolver",20151205);
-    tent.bindValue(":qtd_renovacao",10);
+    tent.bindValue(":data_devedevolver",dataDevolver);
+    tent.bindValue(":qtd_renovacao",15);
 
     if(tent.exec()){
         QMessageBox mens;
@@ -118,8 +143,6 @@ void ProfessorEmprestimoDLG::efetuarEmprestimo(){
         ui->efetuarEmprestimoButton_3->setEnabled(false);
         ui->empButtonProf2->setEnabled(false);
         ui->lineEdit_2->setEnabled(false);
-        ui->tituloLivroPr->setEnabled(false);
-        ui->nomeProf->setEnabled(false);
     }
     else{
         QMessageBox::warning(this,tr("ERRO"),tr("NÃO FOI POSSIVEL EFETUAR O EMPRÉSTIMO"));
